20f5c85a2c8d30bb33f6f102b58eb53be7eb785b
JeffreyIsm <globalmonkey100@gmail.com>
2026-04-17 12:07:30 +0800

    changed folder name

--- Mini-Proj-8/sketch.js
+++ Mini-Proj-8/sketch.js
@@ -1,13 +1,13 @@
 let robotImg;
-//let humanImg;
+let humanImg;
 
 let ball;
 let robotGK;
-//let humanGK;
+let humanGK;
 
 function preload() {
   robotImg = loadImage("assets/RobotGK.png");
-  //humanImg = loadImage("assets/HumanGK.png");
+  humanImg = loadImage("assets/HumanGK.png");
 }
 
 function setup() {
@@ -16,7 +16,7 @@ function setup() {
 
   ball = new Ball();
   robotGK = new RobotGK();
-  //humanGK = new HumanGK();
+  humanGK = new HumanGK();
 }
 
 function draw() {
@@ -25,34 +25,35 @@ function draw() {
   ball.update();
   ball.display();
 
-  //switch which GK to test
-  // humanGK.update();
-  // humanGK.display();
+  //robotGK.update(ball);
+  //robotGK.display();
 
-  robotGK.update(ball);
-  robotGK.display();
+  humanGK.update(ball);
+  humanGK.display();
 }
 
 function keyPressed() {
   let k = key.toLowerCase();
 
-  if (k === 'a' || keyCode === LEFT_ARROW) {
-    ball.shoot("left");
-  }
+  if (ball.isMoving == false){
+    if (k === 'a' || keyCode === LEFT_ARROW) {
+      ball.shoot("left");
+    }
 
-  if (k === 'w' || keyCode === UP_ARROW) {
-    ball.shoot("center");
-  }
+    if (k === 'w' || keyCode === UP_ARROW) {
+      ball.shoot("center");
+    }
 
-  if (k === 'd' || keyCode === RIGHT_ARROW) {
-    ball.shoot("right");
+    if (k === 'd' || keyCode === RIGHT_ARROW) {
+      ball.shoot("right");
+    }
   }
 }
 
 class Ball {
   constructor() {
     this.x = width / 2;
-    this.y = height - 50;
+    this.y = height - 100;
     this.direction = "center";
     this.speed = 10;
     this.isMoving = false;
@@ -74,12 +75,20 @@ class Ball {
       } else if (this.direction === "right") {
         this.x += 3;
       }
+
+      // reset the ball back to position
+      if (this.y < 0-50){
+        this.x = width / 2;
+        this.y = height - 100;
+        this.isMoving = false;
+      }
     }
   }
 
   display() {
-    fill(0);
-    circle(this.x, this.y, 20);
+    fill(255);
+    strokeWeight(5);
+    circle(this.x, this.y, 50);
   }
 }
 
@@ -107,21 +116,32 @@ class RobotGK {
   }
 }
 
-
-/*
-// this is for later, not used yet. Havent tested, just random.
-// the main focus of the complex GK is the robot
 class HumanGK {
   constructor() {
     this.x = width / 2;
     this.y = 100;
+    this.targetX = this.x;
   }
 
-  update() {
-    //random movement every 30 frames
-    if (frameCount % 30 === 0) {
-      this.x = random(100, width - 100);
+  update(ball) {
+    let choice = floor(random(3));
+
+    if (ball.isMoving && this.hasChosen == false){
+      if (choice === 0) {
+        this.targetX = width * 0.35;
+      } else if (choice == 1) {
+        this.targetX = width * 0.65;
+      } else {
+        this.targetX = width / 2;
+      }
+      this.hasChosen = true;
     }
+
+    this.x = lerp(this.x, this.targetX, 0.2);
+
+    if (ball.isMoving == false) {
+      this.hasChosen = false;
+    }  
   }
 
   display() {
@@ -129,4 +149,5 @@ class HumanGK {
     image(humanImg, this.x, this.y, 80, 80);
   }
 }
-  * */
+
+// need to add save mechanism
